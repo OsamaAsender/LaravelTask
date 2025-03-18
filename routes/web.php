790739889da2bf8cc43b4e2ec30243eb655498ef
Osama Asender <103122672+OsamaAsender@users.noreply.github.com>
2025-03-18 08:47:18 +0300

    added my first migration

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/About', function () {
-    return view('AboutUs');
+    $ninjas =[
+        ["name" => "mario", "skill" =>75, "id" => "1"],
+        ["name" => "luigi", "skill" =>85, "id" => "2"]
+    ];
+    return view('AboutUs',["ninjas" => $ninjas]);
 });
 Route::get('/Contact', function () {
     return view('Contactus');
